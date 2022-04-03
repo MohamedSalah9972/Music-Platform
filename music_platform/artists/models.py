924a2f0f3a184d63cda131dcaff8052ecf9bd1ad
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-04-04 00:39:03 +0200

    add Artist model

--- music_platform/artists/models.py
+++ music_platform/artists/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    stage_name = models.CharField(max_length=128, null=False, blank=False, unique=True)
+    social_link = models.URLField(max_length=128, null=False, blank=True, default='')
+
+    class Meta:
+        ordering = ['stage_name']  # ascending
