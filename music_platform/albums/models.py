8a831b86dd5e10f56d963c716a0b8f60a6c264a7
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-04-04 01:08:34 +0200

    create Album model

--- music_platform/albums/models.py
+++ music_platform/albums/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from artists.models import Artist
+from django.utils import timezone
 
-# Create your models here.
+
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, related_name='albums', on_delete=models.CASCADE)  # on_delete=models.CASCADE ??
+    name = models.CharField(max_length=32, default='New Album')
+    creation_datetime = models.DateTimeField(default=timezone.now)
+    release_datetime = models.DateTimeField(blank=False)
+    cost = models.DecimalField(null=False, blank=False, max_digits=10, decimal_places=2)
