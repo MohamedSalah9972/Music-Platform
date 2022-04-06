49dc3453da94ed6548c40d7af11a08bf388970a5
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-04-06 23:05:20 +0200

    change release_datetime field

--- music_platform/albums/models.py
+++ music_platform/albums/models.py
@@ -7,5 +7,5 @@ class Album(models.Model):
     artist = models.ForeignKey(Artist, related_name='albums', on_delete=models.CASCADE)  # on_delete=models.CASCADE ??
     name = models.CharField(max_length=32, default='New Album')
     creation_datetime = models.DateTimeField(default=timezone.now)
-    release_datetime = models.DateTimeField(blank=False)
+    release_datetime = models.DateField(blank=False)
     cost = models.DecimalField(null=False, blank=False, max_digits=10, decimal_places=2)
