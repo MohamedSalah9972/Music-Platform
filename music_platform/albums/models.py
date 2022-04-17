5dea71ae012832e74e64d2936b76c53ac5eac050
MohamedSalah9972 <mohamed.salah9972@gmail.com>
2022-04-17 21:12:49 +0200

    Add is_approved field

--- music_platform/albums/models.py
+++ music_platform/albums/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from artists.models import Artist
 from django.utils import timezone
 
-
 class Album(models.Model):
     artist = models.ForeignKey(Artist, related_name='albums', on_delete=models.CASCADE)  # on_delete=models.CASCADE ??
     name = models.CharField(max_length=32, default='New Album')
     creation_datetime = models.DateTimeField(auto_now_add=True)
     release_datetime = models.DateField(blank=False)
     cost = models.DecimalField(null=False, blank=False, max_digits=10, decimal_places=2)
+    is_approved = models.BooleanField(default=False)
+
