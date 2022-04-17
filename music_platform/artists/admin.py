e15269a026875095d2d2b94a132be06379bcf753
MohamedSalah9972 <mohamed.salah9972@gmail.com>
2022-04-17 22:20:47 +0200

    Add Artist to admin

--- music_platform/artists/admin.py
+++ music_platform/artists/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Artist
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Artist, ArtistAdmin)
