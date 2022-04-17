3a1d5e7d4289b1caa6aa26b9c714306a751afe38
MohamedSalah9972 <mohamed.salah9972@gmail.com>
2022-04-18 00:08:57 +0200

    Add AlbumInline

--- music_platform/artists/admin.py
+++ music_platform/artists/admin.py
@@ -1,15 +1,32 @@
 from django.contrib import admin
+from django.db.models import Count, Q
 
 from .models import Artist
 from albums.models import Album
 
 
+class AlbumInline(admin.StackedInline):  # which is better, StackedInline or TabularInline?
+    model = Album
+
+
 class ArtistAdmin(admin.ModelAdmin):
-    def approved_count(self, artist):  # should I make this function static ?
+    list_display = ('stage_name', 'approved_albums')
+    inlines = [
+        AlbumInline,
+    ]
+
+    def approved_albums(self, artist):  # should I make this function static ?
         num_of_approved = Album.objects.filter(artist=artist, is_approved=True).count()
         return num_of_approved
 
-    list_display = ('stage_name', 'approved_count')
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _approved_albums=Count("albums", filter=Q(albums__is_approved=True)),
+        )
+        return queryset
+
+    approved_albums.admin_order_field = '_approved_albums'
 
 
 admin.site.register(Artist, ArtistAdmin)
