efffee3fb75aadbcf4736f23d1f0aaf18b30936a
MohamedSalah9972 <mohamed.salah9972@gmail.com>
2022-04-17 22:43:19 +0200

    Add approved_count

--- music_platform/artists/admin.py
+++ music_platform/artists/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
 from .models import Artist
+from albums.models import Album
 
 
 class ArtistAdmin(admin.ModelAdmin):
-    pass
+    def approved_count(self, artist):  # should I make this function static ?
+        num_of_approved = Album.objects.filter(artist=artist, is_approved=True).count()
+        return num_of_approved
+
+    list_display = ('stage_name', 'approved_count')
 
 
 admin.site.register(Artist, ArtistAdmin)
