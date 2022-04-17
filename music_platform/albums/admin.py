ad5a23f46a485e4f3b573d94e5ca2ea5300db0c8
MohamedSalah9972 <mohamed.salah9972@gmail.com>
2022-04-17 22:18:20 +0200

    Add help text to Album

--- music_platform/albums/admin.py
+++ music_platform/albums/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
+from django.forms import ModelForm
+
 from .models import Album
+
+
+class AlbumForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AlbumForm, self).__init__(*args, **kwargs)
+        self.fields['is_approved'].help_text = 'Approve the album if its name is not explicit'
+
+    class Meta:
+        model = Album
+        exclude = ()
 
 
 class AlbumAdmin(admin.ModelAdmin):
     readonly_fields = ('creation_datetime',)
-
+    form = AlbumForm
 
 admin.site.register(Album, AlbumAdmin)
