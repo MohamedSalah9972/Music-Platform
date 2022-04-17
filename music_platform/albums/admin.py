4f9c4bef19096fb64966d0a684dd7fdb5de90868
MohamedSalah9972 <mohamed.salah9972@gmail.com>
2022-04-17 21:30:05 +0200

    Add Album to Django Admin

--- music_platform/albums/admin.py
+++ music_platform/albums/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Album
 
-# Register your models here.
+
+class AlbumAdmin(admin.ModelAdmin):
+    readonly_fields = ('creation_datetime',)
+
+
+admin.site.register(Album, AlbumAdmin)
