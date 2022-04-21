ab2873fc31104de5c369560bdd1aec302a0326ad
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-04-21 23:05:32 +0200

    Updates

--- music_platform/artists/models.py
+++ music_platform/artists/models.py
@@ -7,6 +7,6 @@ class Artist(models.Model):
     stage_name = models.CharField(max_length=128, null=False, blank=False, unique=True)
     social_link = models.URLField(max_length=128, null=False, blank=True, default='')
     objects = ArtistQuerySet.as_manager()
-
+    
     class Meta:
         ordering = ['stage_name']  # ascending
