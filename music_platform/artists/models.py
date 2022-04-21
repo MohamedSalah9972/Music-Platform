d8c2a1036b69c20184434345ed7889940af8eec9
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-04-21 20:45:15 +0200

    Add ArtistQuerySet

--- music_platform/artists/models.py
+++ music_platform/artists/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from .managers import ArtistQuerySet
+
 
 class Artist(models.Model):
     stage_name = models.CharField(max_length=128, null=False, blank=False, unique=True)
     social_link = models.URLField(max_length=128, null=False, blank=True, default='')
+    objects = ArtistQuerySet.as_manager()
 
     class Meta:
         ordering = ['stage_name']  # ascending
