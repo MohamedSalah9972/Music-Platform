f5bcfedde3b0fac7f6600f47bc1ecf4321635b3b
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-04-26 23:14:18 +0200

    Add login & signup views

--- music_platform/accounts/views.py
+++ music_platform/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import UserRegisterForm
+from django.views.generic.edit import CreateView
+from django.contrib.auth import views as auth_views
+
+
+class SignUpView(CreateView):
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('signup')
+    form_class = UserRegisterForm
+
+
+class LoginView(auth_views.LoginView):
+    template_name = 'users/login.html'
