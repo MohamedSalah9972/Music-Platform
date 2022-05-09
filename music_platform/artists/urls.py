94c75e2f6d0fda55153449986e57d525dc4074ac
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-09 21:53:42 +0200

    Reformat

--- music_platform/artists/urls.py
+++ music_platform/artists/urls.py
@@ -3,6 +3,5 @@ from . import views
 urlpatterns = [
     path('create/', views.ArtistCreateView.as_view(), name='create_artists'),
     path('', views.ArtistViewSet.as_view(), name="artist-list"),
-    # path('',)
 
 ]
