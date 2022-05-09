15724cc80df68cd82351423e33df940aff7dda90
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-09 15:55:00 +0200

    Add ArtistGenericAPIView

--- music_platform/artists/urls.py
+++ music_platform/artists/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('create/', views.ArtistCreateView.as_view(), name='create_artists'),
-    path('', views.ArtistsDetailView.as_view(), name="artist-list"),
+    path('', views.ArtistViewSet.as_view(), name="artist-list"),
+    # path('',)
 
 ]
--- music_platform/artists/views.py
+++ music_platform/artists/views.py
@@ -4,8 +4,12 @@ from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from rest_framework.response import Response
 
 from .models import Artist
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .serializers import ArtistSerializer
 
 
 @method_decorator(login_required, name='dispatch')
@@ -26,3 +30,24 @@ class ArtistsDetailView(ListView):
         context['artist_list'] = Artist.objects.prefetch_related('albums')
         # how can I get the same result using related_select instead of prefetch_related
         return context
+
+
+from rest_framework import generics, status, views, permissions
+
+
+class ArtistViewSet(generics.GenericAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+    permission_classes = (permissions.AllowAny,)
+    http_method_names = ['get', 'post']
+
+    def get(self, request, format=None):
+        artists = Artist.objects.all()
+        serializer = ArtistSerializer(artists, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = ArtistSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
