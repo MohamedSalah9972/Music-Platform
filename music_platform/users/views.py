421a9054bd3783fa6b643825ea792148b1f5b070
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-13 00:31:46 +0200

    Add patch

--- music_platform/users/views.py
+++ music_platform/users/views.py
@@ -10,29 +10,33 @@ from authentication.serializers import CustomUserSerializer
 from .models import CustomUser
 
 
-class UserDetailView(generics.GenericAPIView):
-    def get_object(self, pk):
-        return get_object_or_404(CustomUser, pk=pk)
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    def get_object(self):
+        return get_object_or_404(CustomUser, pk=self.request.data['id'])
 
     permission_classes = [permissions.AllowAny]
     authentication_classes = [TokenAuthentication]
-
+    serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
 
     def get(self, request, *args, **kwargs):
-        user = self.get_object(request.data['pk'])
+        user = self.get_object()
         return Response({
             "token": AuthToken.objects.create(user)[1],
             "user": CustomUserSerializer(user).data
         })
 
     def put(self, request, *args, **kwargs):
-
-        user = self.get_object(request.data['id'])
-
+        user = self.get_object()
         serializer = CustomUserSerializer(instance=user, data=request.data)
-
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, *args, **kwargs):
+        user_response = self.partial_update(request, *args, **kwargs)
+        return Response({
+            "token": AuthToken.objects.create(self.get_object())[1],
+            "user": user_response.data
+        })
