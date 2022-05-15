d7e3b7e99e59321f6ee7cdc237ad723770dfd341
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-15 17:08:12 +0200

    Add url pk validation

--- music_platform/users/views.py
+++ music_platform/users/views.py
@@ -1,42 +1,74 @@
 from django.shortcuts import render
 from knox.auth import TokenAuthentication
 from knox.models import AuthToken
-from rest_framework import permissions, status, generics
+from rest_framework import permissions, status, generics, viewsets, serializers
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 
 from authentication.serializers import CustomUserSerializer
 from .models import CustomUser
+from .permissions import IsOwnerOrReadOnly
 
 
-class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+class UserAPIView(generics.RetrieveUpdateDestroyAPIView):
+
     def get_object(self):
-        return get_object_or_404(CustomUser, pk=self.request.data['id'])
+        obj = get_object_or_404(CustomUser.objects.all(), pk=self.request.data['id'])
+        return obj
 
-    permission_classes = [permissions.AllowAny]
-    authentication_classes = [TokenAuthentication]
-    serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    authentication_classes = [TokenAuthentication]
+    serializer_class = CustomUserSerializer
+
     def get(self, request, *args, **kwargs):
-        user = self.get_object()
+        pk = self.kwargs.get('pk')
+        user = get_object_or_404(CustomUser.objects.all(), pk=pk)
         return Response({
-            "token": AuthToken.objects.create(user)[1],
             "user": CustomUserSerializer(user).data
         })
 
     def put(self, request, *args, **kwargs):
         user = self.get_object()
+        url_pk = self.kwargs.get('pk')
+        user_pk = self.request.data['id']
+        if url_pk != user_pk:
+            raise serializers.ValidationError({"detail": "You do not have permission to perform this action."})
+
         serializer = CustomUserSerializer(instance=user, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, *args, **kwargs):
+        url_pk = self.kwargs.get('pk')
+        user_pk = self.request.data['id']
+        if url_pk != user_pk:
+            raise serializers.ValidationError({"detail": "You do not have permission to perform this action."})
+
         user_response = self.partial_update(request, *args, **kwargs)
         return Response({
-            "token": AuthToken.objects.create(self.get_object())[1],
             "user": user_response.data
         })
+
+
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def user_list(request):
+    users = CustomUser.objects.all()
+    serializer = CustomUserSerializer(users, many=True)
+    return Response(serializer.data)
+
+# @api_view(['GET'])
+# @permission_classes((permissions.AllowAny,))
+# def user_detail(request, pk):
+#     user = get_object_or_404(CustomUser.objects.all(), pk=pk)
+#     token, created = Token.objects.get_or_create(user=user)
+#     serializer = CustomUserSerializer(user, many=False)
+#     return Response({"token": token.key,
+#                      "user": serializer.data})
