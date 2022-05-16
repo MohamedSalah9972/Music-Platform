ac5b9c2d0c12c7a2b183efcde05af2fcd6c3b891
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-16 13:37:54 +0200

    Display bio as a TextArea

--- music_platform/users/admin.py
+++ music_platform/users/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import CustomUser
+from django import forms
 
 
 class AdminCustomUser(admin.ModelAdmin):
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        formfield = super(AdminCustomUser, self).formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'bio':
+            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
+        return formfield
+
     pass
 
 
