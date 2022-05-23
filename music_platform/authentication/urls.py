45952919ce30c47c77543de588cb40fd2ffa153a
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-23 17:08:57 +0200

    update login url

--- music_platform/authentication/urls.py
+++ music_platform/authentication/urls.py
@@ -4,7 +4,7 @@ from knox import views as knox_views
 from .views import RegisterAPI, LoginAPI
 
 urlpatterns = [
-    path('login/', LoginAPI.as_view(), 'login'),
+    path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
     path('register/', RegisterAPI.as_view()),
