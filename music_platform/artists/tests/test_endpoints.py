6100f744c940be16b371e01a2f7e506f90c42cf5
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-24 14:07:12 +0200

    init

---
+++ music_platform/artists/tests/test_endpoints.py
@@ -0,0 +1,34 @@
+import json
+
+import pytest
+from rest_framework.test import APIClient
+from ..models import Artist
+
+
+@pytest.mark.django_db
+def test_post_artist(auth_client):
+    payload = dict(
+        stage_name="newOne",
+        social_link="https://socail.com"
+    )
+    res = auth_client.post('/artists/', json.dumps(payload), content_type='application/json')
+    assert res.status_code == 200
+    assert res.data['stage_name'] == payload['stage_name']
+
+
+@pytest.mark.django_db
+def test_post_artist_unauthorized(client):
+    payload = dict(
+        stage_name="newOne",
+        social_link="https://socail.com"
+    )
+    res = client.post('/artists/', json.dumps(payload), content_type='application/json')
+    assert res.status_code == 401
+
+
+@pytest.mark.django_db
+def test_get_artist(artist, client):
+    res = client.get('/artists/')
+    assert res.status_code == 200
+    assert res.data[0]['id'] == artist.data['id']
+
