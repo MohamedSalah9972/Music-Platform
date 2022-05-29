0a3a872f6f4e7c0b7176b8994eeeb6e7892778c4
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-29 14:49:36 +0200

    Add albums endpoint url

--- music_platform/albums/urls.py
+++ music_platform/albums/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('create/', views.AlbumCreateView.as_view(), name='create_albums'),
+    path('', views.AlbumViewSet.as_view(), name='album_view_set')
 ]
