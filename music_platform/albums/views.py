a817e4fa47b7e7788cde357113c83b6616719fce
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-29 21:07:46 +0200

    Add post album & AuthenticatedArtist

--- music_platform/albums/views.py
+++ music_platform/albums/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic.edit import CreateView
 from rest_framework import generics, permissions
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
+from rest_framework.status import HTTP_403_FORBIDDEN
 
+from artists.models import Artist
 from .models import Album
-from .serializers import AlbumSerializer
+from .serializers import AlbumSerializer, AlbumPostSerializer
 
 
 @method_decorator(login_required, name='dispatch')
@@ -18,20 +22,31 @@ class AlbumCreateView(CreateView):
         return reverse('artist-list')
 
 
+class AuthenticatedArtist(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+
+        if request.method == 'POST':
+            # is it better to add 'is_artist' to objects manager of user (e.g. user.is_artist()) ?
+            if request.user.is_anonymous or not Artist.objects.filter(user=request.user).exists():
+                raise PermissionDenied()
+
+        return True
+
+
 class AlbumViewSet(generics.GenericAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
     http_method_names = ['get', 'post']
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def has_permission(self, request, view):
-        """
-        Return `True` if permission is granted, `False` otherwise.
-        """
-        print(request)
-        return True
+    permission_classes = [permissions.AllowAny, AuthenticatedArtist]
 
     def get(self, request, format=None):
         artists = Album.objects.approved_albums().all()
         serializer = AlbumSerializer(artists, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = AlbumPostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
