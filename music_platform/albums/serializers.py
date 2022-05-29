be9619a8996940fc83a3f52f0c6c40a51760db72
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-29 14:48:51 +0200

    Add AlbumSerializer

---
+++ music_platform/albums/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Album
+from artists.serializers import ArtistSerializer
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+    artist = ArtistSerializer(read_only=True)
+
+    class Meta:
+        model = Album
+        fields = ['id', 'artist', 'name', 'release_datetime', 'cost']
