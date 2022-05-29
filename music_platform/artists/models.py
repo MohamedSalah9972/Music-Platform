123e90273ce6ff5449f6115a7e42972b7a7c3210
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-29 13:36:27 +0200

    Add user field in artist

--- music_platform/artists/models.py
+++ music_platform/artists/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 from .managers import ArtistQuerySet
 
 
@@ -7,6 +7,7 @@ class Artist(models.Model):
     stage_name = models.CharField(max_length=128, null=False, blank=False, unique=True)
     social_link = models.URLField(max_length=128, null=False, blank=True, default='')
     objects = ArtistQuerySet.as_manager()
-    
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, unique=True, on_delete=models.CASCADE)
+
     class Meta:
         ordering = ['stage_name']  # ascending
