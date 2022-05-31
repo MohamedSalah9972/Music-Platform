4d3ec8da2b3f4435a374fb8c132d740201733bc0
Mohammed Salah <mohamed.salah9972@gmail.com>
2022-05-31 15:56:39 +0200

    Add django_filters

--- music_platform/music_platform/settings.py
+++ music_platform/music_platform/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'users',
     'knox',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -145,8 +146,13 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.SessionAuthentication',
         'knox.auth.TokenAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': (
+                'django_filters.rest_framework.DjangoFilterBackend',
+
+            ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+
 
 }
 
